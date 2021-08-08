2be22567b4e097e6cb5fba0c3b071c204a046293
Miral Suthar <57826091+miralsuthar@users.noreply.github.com>
2021-08-08 23:12:22 +0530

    added create order and get order functionality

---
+++ pages/api/orders.ts
@@ -0,0 +1,46 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
+
+export async function getOrders() {
+  let { data: orders, error } = await supabase.from('orders').select('*');
+
+  if (error) {
+    return {
+      success: false,
+      error,
+      orders: [],
+    };
+  }
+  return {
+    success: true,
+    orders,
+    error: null,
+  };
+}
+
+export async function createOrder(order) {
+  const { data, error } = await supabase.from('orders').insert([order]);
+  return { error, data };
+}
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    const output = await getOrders();
+    if (!output.success) {
+      return res.status(500).json(output);
+    }
+    res.status(200).json(output);
+  }
+
+  if (req.method === 'POST') {
+    const output = await createOrder(req.body);
+    if (output.error) {
+      return res.status(500).json(output);
+    }
+    res.status(200).json(output);
+  }
+}
