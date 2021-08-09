c1cd848454794d8a88e79762c3bc8b87341f78cf
Nazeeh21 <nazeehvahora.786@gmail.com>
2021-08-09 21:24:32 +0530

    Fix bug of fetching listingData

--- pages/orders.tsx
+++ pages/orders.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { getOrders, getListingData } from './api/orders';
 import { OrderCard } from '../components/OrderCard';
+
 const Index = ({ orders }) => {
+  useEffect(() => {
+    Promise.all(
+      orders.map(async (order) => {
+        const { data, error } = await getListingData(order.listing_id);
+
+        if (!error && data) {
+          order.listingData = data;
+        }
+      })
+    );
+  }, [orders]);
+
   return (
-    <div className="flex flex-col gap-6">
+    <div className='flex flex-col gap-6'>
       {orders &&
-        orders.map(async (order, index) => {
-          const { reqListings } = await getListingData(order.id);
+        orders.map((order, index) => {
+          console.log('order from orders.tsx: ', order)
           return (
-            <OrderCard
-              key={index}
-              id={order.id}
-              customerEmail={order.customer_email}
-              shippingCode={order.shipping_to_pincode}
-              qty={order.qty}
-              title={reqListings[0].title}
-              sku={reqListings[0].sku}
-              payableAmount={order.payable_amount}
-            />
+            <div key={index}>
+              <OrderCard
+                id={order.id}
+                customerEmail={order.customer_email}
+                shippingCode={order.shipping_to_pincode}
+                qty={order.qty}
+                title={order.listingData.title}
+                sku={order.listingData.sku}
+                payableAmount={order.payable_amount}
+              />
+            </div>
           );
         })}
     </div>
@@ -26,7 +40,12 @@ const Index = ({ orders }) => {
 
 export async function getServerSideProps() {
   const { orders } = await getOrders();
-  console.log(orders);
+  
+  for await (let order of orders) {
+    const {data} = await getListingData(order.listing_id)
+    order.listingData = data;
+  }
+  
   return {
     props: {
       orders,
