866f9116112cd7353952ddc3e8af3856bec5e0f0
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-08-09 06:12:31 +0530

    Re-factor

---
+++ types.ts
@@ -0,0 +1,19 @@
+export interface IListing {
+  id: string;
+  created_by: string;
+  title: string;
+  description: string;
+  price: number;
+  sku: string;
+  image_file_names?: string[];
+  images?: string[];
+}
+
+export interface IOrder {
+  listing_id: string;
+  customer_email: string;
+  shipping_to_pincode: string;
+  qty: number;
+  payment_done: boolean;
+  delivery_done: boolean;
+}
