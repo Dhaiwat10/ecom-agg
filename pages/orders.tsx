1ad423baa506d0d8d1762876dbda0b0c68b6f2ae
Miral Suthar <57826091+miralsuthar@users.noreply.github.com>
2021-08-10 00:21:01 +0530

    add the getListingImages function in oreders

--- pages/orders.tsx
+++ pages/orders.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { getOrders, getListingData } from './api/orders';
 import { OrderCard } from '../components/OrderCard';
+import { getListingImages } from './api/listings';
 
 const Index = ({ orders }) => {
   useEffect(() => {
@@ -16,13 +17,14 @@ const Index = ({ orders }) => {
   }, [orders]);
 
   return (
-    <div className='flex flex-col gap-6'>
+    <div className="mx-auto grid lg:grid-cols-2 flex-col gap-6">
       {orders &&
         orders.map((order, index) => {
-          console.log('order from orders.tsx: ', order)
+          console.log('order from orders.tsx: ', order);
           return (
             <div key={index}>
               <OrderCard
+                image={order.image}
                 id={order.id}
                 customerEmail={order.customer_email}
                 shippingCode={order.shipping_to_pincode}
@@ -40,12 +42,20 @@ const Index = ({ orders }) => {
 
 export async function getServerSideProps() {
   const { orders } = await getOrders();
-  
+
   for await (let order of orders) {
-    const {data} = await getListingData(order.listing_id)
+    const { data } = await getListingData(order.listing_id);
+    const images = await getListingImages(data);
+    if (images[0] === undefined) {
+      order.image = '';
+    } else {
+      order.image = images[0];
+    }
+
+    console.log('images: ', order.image);
     order.listingData = data;
   }
-  
+
   return {
     props: {
       orders,
